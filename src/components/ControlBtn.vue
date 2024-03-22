9faf810829511a90e273eee4d00f48318c5873b7
zhongziqi233 <2673122425@qq.com>
2024-03-22 10:16:32 +0800

    init

---
+++ src/components/ControlBtn.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="btnSubject" :title=title :style="dynamicStyle">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "ControlBtn",
+  props: {
+    title: String,
+    bc: String,
+    bi: String,
+  },
+  computed: {  
+    dynamicStyle() {
+      if (this.bc == "") {
+        return {
+          backgroundImage: this.bi,
+        };
+      } else {
+        return {
+          backgroundColor: this.bc,
+        };
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+#btnSubject {
+  cursor: pointer;
+  user-select:none;
+  display: flex;
+  box-sizing: border-box;
+  margin: 5px 10px;
+  padding: 5px 5px;
+  border-radius: 5px;
+  height: 7%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  transition: 0.5s;
+}
+#btnSubject:hover {
+  margin: 0 0;
+  padding: 0 0;
+  transition: 0.5s;
+}
+</style>
---
+++ src/components/MainViewport.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="mainViewport" :style="dynamicStyle">
+    <ImageToParticle/>
+  </div>
+</template>
+<script>
+import ImageToParticle from "./ImageToParticle.vue";
+export default {
+  name: "MainViewport",
+  components: { ImageToParticle },
+  computed: {
+    dynamicStyle() {
+      return {
+        backgroundColor: this.$store.getters.getBackgroundColor
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+#mainViewport {
+  width: 100%;
+  height: 100%;
+}
+</style>
